695b94f8f07d21ef2267f62df7430de010e7e30e
gabriela-carnevali <gabriela.c.lima10@aluno.senai.br>
2026-09-26 14:07:12 -0300

    Edição do index.js

--- back-end/src/configuracoes/index.js
+++ back-end/src/configuracoes/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const path = require('node:path');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
 if (!process.env.JWT_SECRET) {
   throw new Error('JWT_SECRET não definido. Configure essa variável antes de iniciar o servidor.');
